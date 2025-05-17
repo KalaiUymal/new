0b55afaac2e32151ee323073ca22c1170719fee4
KalaiUymal <jeevakalai7708@gmail.com>
2025-05-17 11:01:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from streamlit_option_menu import option_menu
 
 name=st.text_input("Enter your name")
 age=st.number_input("Enter your age")
-city=st.selectbox("select your city", ["Mumbai", "Delhi", "Chennai", "Kolkata"])
+
